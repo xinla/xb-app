7d26cffdf27c8ff3c994d593e02273058462a65d
xinla <1570728529@qq.com>
2019-06-28 21:02:31 +0800

    product environment setting

--- src/config.js
+++ src/config.js
@@ -3,9 +3,9 @@ const config = {
     // dev: 'http://192.168.1.55', // 虚拟机
     dev: 'http://192.168.1.11:8888/api/policy-service/v1', // wangdong
     domain2: process.env.NODE_ENV === 'development' ? 'http://192.168.1.32:8115/v1' : '', // liusiyuan
-    dev3: 'http://192.168.1.11:8888',
-    domain4: process.env.NODE_ENV === 'development' ? 'http://192.168.1.150:8888/' : '', // liushaung~
-    pro: 'https://produce.com'
+    dev3:  process.env.NODE_ENV === 'development' ? 'http://192.168.1.11:8888' : 'https://gateway.visualinsur.cn:8888',
+    domain4: process.env.NODE_ENV === 'development' ? 'http://192.168.1.150:8888/' : 'https://gateway.visualinsur.cn:8888', // liushaung~
+    pro: 'https://47.98.153.101:8888' // wangdong
   },
 }
 
