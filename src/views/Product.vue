9ed8082eab4b582ac0ff889ffb810a73b8ebf661
xinla <1570728529@qq.com>
2019-12-07 11:22:26 +0800

    editor...

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -9,27 +9,27 @@
     </div>
 
     <div ref="tab-wrap" class="tab-wrap">
-      <div class="tab-title-wrap">
-        <!-- <div
-          :class="['tab-title', {'active': tabActive == index}]"
-          v-for="(item, index) in product.insurableInterest"
-          v-if="item.scheduleName"
-          :key="index"
-          @click="tabActive = index"
-        >{{item.scheduleName}}</div> -->
-        <div class="tab-title active">保障利益</div>
-      </div>
-      <mt-tab-container v-model="tabActive" swipeable>
-        <mt-tab-container-item :id="0"
-        >
-          <ul class="tab1">
-            <li v-for="(unit, unique) in product.insurableInterest" :key="unique">
-              <span>{{unit.title}}</span>
-              <span class="fr">{{unit.algorithm}}</span>
-            </li>
-          </ul>
-        </mt-tab-container-item>
-      </mt-tab-container>
+      <template v-if="product.insurancePlan">
+        <div class="tab-title-wrap">
+          <div
+            :class="['tab-title', {'active': tabActive == index}]"
+            v-for="(item, index) in product.insurancePlanContent"
+            :key="index"
+            @click="tabActive = index"
+          >{{item.name}}</div>
+          <!-- <div class="tab-title active">保障利益</div> -->
+        </div>
+        <mt-tab-container v-model="tabActive" swipeable>
+          <mt-tab-container-item :id="0">
+            <ul class="tab1">
+              <li v-for="(unit, unique) in product.insurancePlanContent" :key="unique">
+                <span>{{planOptionList[unit.option]}}</span>
+                <span class="fr">{{unit.amount}}{{['万元', '元', '元/天'][unit.unit]}}</span>
+              </li>
+            </ul>
+          </mt-tab-container-item>
+        </mt-tab-container>
+      </template>
     </div>
 
     <ul ref="tab2-title-wrap" class="tab2-title-wrap wrapper">
@@ -53,10 +53,19 @@
     <mt-tab-container v-model="active" style="margin-top: .94rem;min-height: 1rem;">
       <!-- 保险详情 -->
       <mt-tab-container-item id="1">
-        <div v-if="product.imageText" v-html="product.imageText"></div>
-        <template v-else>
-        <img v-for="(item, index) of product.descPicture" :src="item" :key="index" alt />
+        <!-- 优先展示图集 -->
+        <template v-if="product.descPicture">
+          <img v-for="(item, index) of product.descPicture" :src="item" :key="index" alt />
         </template>
+        <!-- <ckeditor
+        v-else-if="product.imageText"
+          :editor="editor"
+          v-model="product.imageText"
+          :config="editorConfig"
+          :disabled="true"
+        ></ckeditor> -->
+        <div v-else-if="product.imageText" class="insurance-text btn-bottom editor" v-html="product.imageText"></div>
+        <div class="ac" v-else><br>暂无详情介绍，等待后台更新。</div>
         <!-- </ul> -->
       </mt-tab-container-item>
       <!-- 费率表 -->
@@ -242,14 +251,28 @@
       </mt-tab-container-item>
       <!-- 投保规则 -->
       <mt-tab-container-item id="3">
-        <!-- <div class="insurance-text" v-html="product.underwritingRulesText"></div> -->
-        <img
-          v-for="(item, index) in product.insuranceRulePdf"
-          class="policy-wording-images"
-          preview="1"
-          :src="item"
-          :key="index"
-        />
+        <div
+          v-if="product.underwritingRulesText"
+          class="insurance-text btn-bottom editor"
+          v-html="product.underwritingRulesText"
+        ></div>
+        <!-- <ckeditor
+        v-if="product.underwritingRulesText"
+          :editor="editor"
+          v-model="product.underwritingRulesText"
+          :config="editorConfig"
+          :disabled="true"
+        ></ckeditor> -->
+        <template v-else-if="product.insuranceRulePdf">
+          <img
+            v-for="(item, index) in product.insuranceRulePdf"
+            class="policy-wording-images"
+            preview="1"
+            :src="item"
+            :key="index"
+          />
+        </template>
+        <div class="ac" v-else><br>暂无规则介绍，等待后台更新。</div>
       </mt-tab-container-item>
       <!-- 产品条款 -->
       <mt-tab-container-item id="4">
@@ -258,6 +281,7 @@
                 <p>This browser does not support PDFs. Please download the PDF to view it: <a :href="item.url">Download PDF</a>.</p>
             </embed>
         </object>-->
+        <!-- <div v-if="product.insuranceLiability" class="insurance-text" v-html="product.insuranceLiability"></div> -->
         <img
           v-for="(item, index) in product.insuranceLiabilityPdf"
           class="policy-wording-images"
@@ -277,9 +301,25 @@ import {
   getProductRateDetail
 } from "@/api/product";
 import { getWeChatSign } from "@/api/weChatShare";
+
+import CKEditor from "@ckeditor/ckeditor5-vue";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import "@ckeditor/ckeditor5-build-classic/build/translations/zh-cn";
+
 export default {
+  components: {
+    ckeditor: CKEditor.component
+  },
   data() {
     return {
+      editor: ClassicEditor,
+      editorConfig: {
+        // The configuration of the editor.
+        language: "zh-cn",
+        toolbar: {
+          items: []
+        }
+      },
       tabActive: 0,
       active: "1",
       offsetTop: 0,
@@ -373,7 +413,14 @@ export default {
         //   key: "paymentMethod"
         // }
       ],
-      listRates: []
+      listRates: [],
+      planOptionList: Object.freeze({
+        0: "无关连",
+        1: "有社保",
+        2: "无社保",
+        3: "男性",
+        4: "女性"
+      })
     };
   },
   mounted() {
@@ -387,54 +434,53 @@ export default {
     getData(query) {
       // console.log(query.token)
       // 获取产品详情
-      getProductDetail(query)
-        .then(res => {
-          res.insurableInterest = JSON.parse(res.insurableInterest) || [];
-          // res.attachment.productCourse =
-          //   JSON.parse(res.attachment.productCourse) || [];
-          this.product = res;
-          // 设置文档标题
-          document.title = res.product.productFullName;
-          // 图集转换
-          typeof this.product.descPicture === "string" &&
-            (this.product.descPicture = this.product.descPicture.split(","));
-          // 投保规则图片转换
-          typeof this.product.insuranceRulePdf === "string" &&
-            (this.product.insuranceRulePdf = this.product.insuranceRulePdf.split(
-              ","
-            ));
-// 产品条款图片转换
-          typeof this.product.insuranceLiabilityPdf === "string" &&
-            (this.product.insuranceLiabilityPdf = this.product.insuranceLiabilityPdf.split(
-              ","
-            ));
-          // console.log("ProductDetail: ", res);
-          this.$nextTick(() => {
-            this.offsetTop =
-              this.$refs["tab2-title-wrap"].offsetTop -
-              this.$refs["title"].clientHeight;
-            this.offsetTopTitle = this.$refs["title"].offsetTop;
+      getProductDetail(query).then(res => {
+        res.insurancePlanContent = JSON.parse(res.insurancePlanContent) || [];
+        // res.attachment.productCourse =
+        //   JSON.parse(res.attachment.productCourse) || [];
+        this.product = res;
+        // 设置文档标题
+        document.title = res.product.productFullName;
+        // 图集转换
+        typeof this.product.descPicture === "string" &&
+          (this.product.descPicture = this.product.descPicture.split(","));
+        // 投保规则图片转换
+        typeof this.product.insuranceRulePdf === "string" &&
+          (this.product.insuranceRulePdf = this.product.insuranceRulePdf.split(
+            ","
+          ));
+        // 产品条款图片转换
+        typeof this.product.insuranceLiabilityPdf === "string" &&
+          (this.product.insuranceLiabilityPdf = this.product.insuranceLiabilityPdf.split(
+            ","
+          ));
+        // console.log("ProductDetail: ", res);
+        this.$nextTick(() => {
+          this.offsetTop =
+            this.$refs["tab2-title-wrap"].offsetTop -
+            this.$refs["title"].clientHeight;
+          this.offsetTopTitle = this.$refs["title"].offsetTop;
 
-            // 动态设置标题css
-            this.$refs["tab-wrap"].style.marginTop =
-              this.$refs["title"].clientHeight + "px";
-            this.$refs["title"].style.marginBottom =
-              -this.$refs["title"].clientHeight + "px";
-          });
-          // return getWeChatSign(location.href);
-        })
-        // .then(res => {
-        //   console.log(res);
-        //   res = JSON.parse(res);
-        //   wx.config({
-        //     debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-        //     appId: "wx02b7033c0c901dd7", // 必填，公众号的唯一标识
-        //     timestamp: res.timestamp, // 必填，生成签名的时间戳
-        //     nonceStr: res.nonceStr, // 必填，生成签名的随机串
-        //     signature: res.signature, // 必填，签名
-        //     jsApiList: ["updateAppMessageShareData", "updateTimelineShareData"] // 必填，需要使用的JS接口列表
-        //   });
-        // });
+          // 动态设置标题css
+          this.$refs["tab-wrap"].style.marginTop =
+            this.$refs["title"].clientHeight + "px";
+          this.$refs["title"].style.marginBottom =
+            -this.$refs["title"].clientHeight + "px";
+        });
+        // return getWeChatSign(location.href);
+      });
+      // .then(res => {
+      //   console.log(res);
+      //   res = JSON.parse(res);
+      //   wx.config({
+      //     debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      //     appId: "wx02b7033c0c901dd7", // 必填，公众号的唯一标识
+      //     timestamp: res.timestamp, // 必填，生成签名的时间戳
+      //     nonceStr: res.nonceStr, // 必填，生成签名的随机串
+      //     signature: res.signature, // 必填，签名
+      //     jsApiList: ["updateAppMessageShareData", "updateTimelineShareData"] // 必填，需要使用的JS接口列表
+      //   });
+      // });
 
       //自定义分享 需在用户可能点击分享按钮前就先调用
       // wx.ready(() => {
@@ -542,6 +588,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
+@import url('../styles/editor.css');
 .main {
   overflow: scroll;
 }
@@ -587,14 +634,15 @@ export default {
 }
 .tab2-title-wrap {
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   background: #fff;
   border-bottom: solid 1px #f1f3f5;
   line-height: 0.8rem;
   color: #a6abb7;
   z-index: 1;
-  width: 100%;
+  padding: 0 0.2rem;
   margin-bottom: -0.94rem;
+  width: 100%;
   .current {
     color: #444;
   }
@@ -720,5 +768,10 @@ table {
 /deep/.mint-spinner-fading-circle {
   margin: 10px auto;
 }
+
+/deep/.ck.ck-editor {
+    line-height: .44rem;
+}
+
 </style>
 
